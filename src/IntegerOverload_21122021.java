285763a163a8184282219b9220a12e9262dd6986
tamer.kacak <tamercankacak@gmail.com>
2021-12-21 13:10:39 +0300

    Initial commit

---
+++ src/IntegerOverload_21122021.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+//https://www.hackerrank.com/challenges/mini-max-sum/problem
+public class IntegerOverload_21122021 {
+  public static void main(String[] args) {
+    miniMaxSum(
+        new ArrayList<Integer>() {
+          {
+            add(256741038);
+            add(623958417);
+            add(467905213);
+            add(714532089);
+            add(938071625);
+          }
+        });
+  }
+
+  public static void miniMaxSum(List<Integer> arr) {
+    //Integer List Sorting
+    arr = arr.stream().sorted().collect(Collectors.toList());
+
+    //Integer List to Long List => arr.stream().mapToLong(Integer::longValue)
+    long total = arr.stream().mapToLong(Integer::longValue).sum();
+
+    System.out.println(
+        (total - arr.get(arr.size() - 1))
+            + " "
+            + (total - arr.get(0)));
+  }
+}
