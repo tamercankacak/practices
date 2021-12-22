08226ba8c979af566c2248684e87f54b62d2b822
tamer.kacak <tamercankacak@gmail.com>
2021-12-22 12:04:26 +0300

    chore: elseif-stdout

---
+++ src/Java/_20211222_stdin.java
@@ -0,0 +1,18 @@
+package Java;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+public class _20211222_stdin {
+  public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner scanner = new Scanner(System.in);
+    String first = scanner.nextLine();
+    String second = scanner.nextLine();
+    String third = scanner.nextLine();
+    //int integerValue = scanner.nextInt();
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(third);
+  }
+}
---
+++ src/Java/_20211222_stdin2.java
@@ -0,0 +1,21 @@
+package Java;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+public class _20211222_stdin2 {
+  public static void main(String[] args) {
+    //
+    Scanner scanner = new Scanner(System.in);
+
+    int intValue = scanner.nextInt();
+    scanner.nextLine();
+    double doubleValue = scanner.nextDouble();
+    scanner.nextLine();
+    String stringValue = scanner.nextLine();
+
+    System.out.println("String: " + stringValue);
+    System.out.println("Double: " + doubleValue);
+    System.out.println("Int: " + intValue);
+  }
+}
