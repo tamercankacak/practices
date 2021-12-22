33770626b9371013781f6fe4a5668f63e4960971
tamer.kacak <tamercankacak@gmail.com>
2021-12-22 10:28:53 +0300

    chore: Grading Students Problem

---
+++ src/_20211222_Modulus.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+// https://www.hackerrank.com/challenges/grading/problem
+public class _20211222_Modulus {
+  public static void main(String[] args) {
+    List<Integer> list =
+        new ArrayList<Integer>() {
+          {
+            add(73);
+            add(67);
+            add(38);
+            add(33);
+          }
+        };
+    System.out.println(gradingStudents(list));
+  }
+
+  public static List<Integer> gradingStudents(List<Integer> grades) {
+    for (int i = 0; i < grades.size(); i++) {
+      if (grades.get(i) < 38 || grades.get(i) % 5 < 3) continue;
+      if (grades.get(i) % 5 != 0) {
+        grades.set(i, grades.get(i) + (5 - (grades.get(i) % 5)));
+      }
+    }
+
+    return grades;
+  }
+}
