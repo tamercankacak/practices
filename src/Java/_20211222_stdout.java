0af7b606ddc944ee83c2c279ced65642476b2a4f
tamer.kacak <tamercankacak@gmail.com>
2021-12-22 11:07:18 +0300

    chore: Stdout

--- src/Java/_20211222_stdout.java
+++ src/Java/_20211222_stdout.java
@@ -1,9 +1,9 @@
 package Java;
-
+// https://www.hackerrank.com/challenges/welcome-to-java/problem
 public class _20211222_stdout {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     System.out.println("Hello, World.");
     System.out.println("Hello, Java.");
-    }
+  }
 }
