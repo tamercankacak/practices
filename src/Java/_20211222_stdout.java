15a2238668ec4ace9220925c217d2080b5aa6ed3
tamer.kacak <tamercankacak@gmail.com>
2021-12-22 11:06:00 +0300

    chore: Stdout

---
+++ src/Java/_20211222_stdout.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class _20211222_stdout {
+    public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+    }
+}
