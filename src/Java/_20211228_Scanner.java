f76db44b332097c60d8507e33b3c65db8854ee91
tamer.kacak <tamercankacak@gmail.com>
2021-12-28 17:41:54 +0300

    chore: java-int-to-string

---
+++ src/Java/_20211228_Scanner.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-int-to-string/problem
+public class _20211228_Scanner {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int value = scanner.nextInt();
+
+    System.out.println("Good job");
+  }
+}
