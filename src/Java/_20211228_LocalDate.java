120d497960b02a74fd61b7b4ce083bb1eef2c4c6
tamer.kacak <tamercankacak@gmail.com>
2021-12-28 17:41:36 +0300

    chore: java-date-and-time

---
+++ src/Java/_20211228_LocalDate.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.time.LocalDate;
+
+// https://www.hackerrank.com/challenges/java-date-and-time/problem
+public class _20211228_LocalDate {
+  public static void main(String[] args) {
+    System.out.println(findDay(8, 5, 2015));
+  }
+
+  public static String findDay(int month, int day, int year) {
+    LocalDate localDate = LocalDate.of(year, month, day);
+    return localDate.getDayOfWeek().toString();
+  }
+}
