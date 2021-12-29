96125c073a3548732ea007adb50cec68d6847158
tamer.kacak <tamercankacak@gmail.com>
2021-12-29 14:48:29 +0300

    chore: java-currency-formatter

---
+++ src/Java/Solution.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-currency-formatter/problem
+public class Solution {
+  public static void main(String[] args) {
+    //
+    Scanner scanner = new Scanner(System.in);
+    String value = scanner.nextLine();
+
+
+
+  }
+}
