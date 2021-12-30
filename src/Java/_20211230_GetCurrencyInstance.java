b1b3ad4912907134be7115f7b378b21ccdb83a4b
tamer.kacak <tamercankacak@gmail.com>
2021-12-30 15:30:10 +0300

    chore: java-currency-formatter

---
+++ src/Java/_20211230_GetCurrencyInstance.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-currency-formatter/problem
+public class _20211230_GetCurrencyInstance {
+  public static void main(String[] args) {
+    //
+    Scanner scanner = new Scanner(System.in);
+    double payment = scanner.nextDouble();
+    scanner.close();
+
+    Locale indiaLocale = new Locale("en", "IN");
+
+    NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+    NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+    NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+    NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+    System.out.println("US: " + us.format(payment));
+    System.out.println("India: " + india.format(payment));
+    System.out.println("China: " + china.format(payment));
+    System.out.println("France: " + france.format(payment));
+  }
+}
