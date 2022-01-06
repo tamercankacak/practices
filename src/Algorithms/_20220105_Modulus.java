4c1f7f15c95b600f08bf199688e43c36f9c75f4c
tamer.kacak <tamercankacak@gmail.com>
2022-01-06 13:16:51 +0300

    chore: Modulus

---
+++ src/Algorithms/_20220105_Modulus.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+// https://www.hackerrank.com/challenges/drawing-book/problem
+public class _20220105_Modulus {
+  public static void main(String[] args) {
+    int result = pageCount(5, 4);
+    System.out.println(result);
+  }
+
+  public static int pageCount(int n, int p) {
+    int firstLong = p / 2;
+    int lastLong = n % 2 == 0 ? (n - 1 - p) / 2 + 1 : (n - p) / 2;
+    return n == p ? 0 : firstLong < lastLong ? firstLong : lastLong;
+  }
+}
