eb133fd35639b47b253edda96320ce2ba8e025f9
tamer.kacak <tamercankacak@gmail.com>
2022-01-06 13:15:40 +0300

    chore: StringReverse

---
+++ src/Java/_20220104_StringReverse.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-string-reverse/problem
+public class _20220104_StringReverse {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String A = sc.next();
+    String reversedA = "";
+    for (int i = A.length() - 1; i >= 0; --i) {
+      reversedA = reversedA.concat(String.valueOf(A.charAt(i)));
+    }
+    System.out.println(A.equals(reversedA) ? "Yes" : "No");
+  }
+}
