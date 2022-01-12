b6b27de6d790473893355debcc023fa4e5f8028e
tamer.kacak <tamercankacak@gmail.com>
2022-01-12 23:50:45 +0300

    chore: algorithm continue

---
+++ src/algorithms/Solution.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+import java.util.List;
+
+/** https://www.hackerrank.com/challenges/magic-square-forming/problem */
+public class Solution {
+  public static void main(String[] args) {}
+
+  public static int formingMagicSquare(List<List<Integer>> s) {
+
+    return 0;
+  }
+}
