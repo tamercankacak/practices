5af3e4a488494501c324fb5ceef9e5cbf493f85d
tamer.kacak <tamercankacak@gmail.com>
2022-01-14 20:20:44 +0300

    chore: java continue

--- src/javasolution/Javasolution.java
+++ src/javasolution/Javasolution.java
@@ -2,5 +2,7 @@ package javasolution;
 
 /** https://www.hackerrank.com/challenges/valid-username-checker/problem */
 public class Javasolution {
+  public static final String regularExpression = null;
+
   public static void main(String[] args) {}
 }
