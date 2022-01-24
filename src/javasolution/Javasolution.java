b502ae270b4e39fc17d7407fe39c3bcff626c39e
tamer.kacak <tamercankacak@gmail.com>
2022-01-24 18:21:30 +0300

    chore: java practice changed

--- src/javasolution/Javasolution.java
+++
@@ -1,8 +0,0 @@
-package javasolution;
-
-/** https://www.hackerrank.com/challenges/valid-username-checker/problem */
-public class Javasolution {
-  public static final String regularExpression = null;
-
-  public static void main(String[] args) {}
-}
