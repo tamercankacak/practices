563fbb37d5690837250c4ff239bc74bd033d87ae
tamer.kacak <tamercankacak@gmail.com>
2022-01-24 18:21:14 +0300

    chore: java practice changed

---
+++ src/javasolution/Solution.java
@@ -0,0 +1,13 @@
+package javasolution;
+
+/** https://www.hackerrank.com/challenges/java-1d-array-introduction/problem */
+public class Solution {
+  /**
+   * solution.
+   *
+   * @param args args.
+   */
+  public static void main(String[] args) {
+    //
+  }
+}
