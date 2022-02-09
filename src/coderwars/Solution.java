7e7945c6aac5ee617813df1220ae352c52614450
tamer.kacak <tamercankacak@gmail.com>
2022-02-09 16:43:41 +0300

    chore: Solution

---
+++ src/coderwars/Solution.java
@@ -0,0 +1,10 @@
+package coderwars;
+
+/** This is main method class. */
+public class Solution {
+  public static void main(String[] args) {
+    //
+    System.out.println("Welcome my practice world !");
+    //
+  }
+}
