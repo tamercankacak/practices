5f9a6ca6712c597f3000b9a7dd4b6b2d4d91c024
tamer.kacak <tamercankacak@gmail.com>
2022-02-10 09:22:37 +0300

    chore: YesOrNo

---
+++ src/coderwars/YesOrNo.java
@@ -0,0 +1,19 @@
+package coderwars;
+
+/**
+ * https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+ *
+ * @author tamercankacak
+ */
+public class YesOrNo {
+
+  /**
+   * This method.
+   *
+   * @param b boolean value.
+   * @return returns "Yes" if it is true, "No" if it is false.
+   */
+  public static String boolToWord(boolean b) {
+    return b ? "Yes" : "No";
+  }
+}
