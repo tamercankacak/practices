9304a946f278e3ca48f802ab82624b854175a0a1
tamer.kacak <tamercankacak@gmail.com>
2022-02-12 12:38:13 +0300

    chore: MethodTests

--- src/coderwars/MethodTests.java
+++ src/coderwars/MethodTests.java
@@ -1,6 +1,7 @@
 package coderwars;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.util.Arrays;
 import org.junit.Test;
@@ -13,9 +14,35 @@ import org.junit.Test;
 public class MethodTests {
 
   @Test
+  public void testSimpleDirReduc() {
+    assertArrayEquals(
+        "\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"",
+        new String[] {"WEST"},
+        DirReduction.dirReduc(
+            new String[] {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+    assertArrayEquals(
+        "\"NORTH\",\"SOUTH\",\"SOUTH\",\"EAST\",\"WEST\",\"NORTH\"",
+        new String[] {},
+        DirReduction.dirReduc(new String[] {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"}));
+  }
+
+  @Test
+  public void Test1() {
+    assertEquals(-1, Sum.GetSum(0, -1));
+    assertEquals(1, Sum.GetSum(0, 1));
+  }
+
+  @Test
+  public void tests() {
+    assertEquals("67", ConvertaNumbertoString.numberToString(67));
+    assertEquals("123", ConvertaNumbertoString.numberToString(123));
+    assertEquals("999", ConvertaNumbertoString.numberToString(999));
+  }
+
+  @Test
   public void testExample() {
-    int[] exampleTest1 = {2,6,8,-10,3};
-    int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781};
+    int[] exampleTest1 = {2, 6, 8, -10, 3};
+    int[] exampleTest2 = {206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781};
     int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
     assertEquals(3, FindOutlier.find(exampleTest1));
     assertEquals(206847684, FindOutlier.find(exampleTest2));
@@ -25,16 +52,19 @@ public class MethodTests {
   @Test
   public void testSolution() {
     assertEquals("############5616", Maskify.maskify("4556364607935616"));
-    assertEquals("#######5616",      Maskify.maskify(     "64607935616"));
-    assertEquals("1",                Maskify.maskify(               "1"));
-    assertEquals("",                 Maskify.maskify(                ""));
+    assertEquals("#######5616", Maskify.maskify("64607935616"));
+    assertEquals("1", Maskify.maskify("1"));
+    assertEquals("", Maskify.maskify(""));
 
     // "What was the name of your first pet?"
-    assertEquals("##ippy",                                    Maskify.maskify("Skippy")                                  );
-    assertEquals("####################################man!",  Maskify.maskify("Nananananananananananananananana Batman!"));
+    assertEquals("##ippy", Maskify.maskify("Skippy"));
+    assertEquals(
+        "####################################man!",
+        Maskify.maskify("Nananananananananananananananana Batman!"));
   }
 
-  @Test public void test4a() {
+  @Test
+  public void test4a() {
     assertEquals("aaaa", StringRepeat.repeatStr(4, "a"));
     assertEquals("HelloHelloHello", StringRepeat.repeatStr(3, "Hello"));
     assertEquals("", StringRepeat.repeatStr(5, ""));
