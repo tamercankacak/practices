b9f2fb1df65a77a33a11594d24b317dc7bbf4aa8
tamer.kacak <tamercankacak@gmail.com>
2022-02-19 12:14:39 +0300

    chore: KeepHydrated

---
+++ src/coderwars/KeepHydrated.java
@@ -0,0 +1,12 @@
+package coderwars;
+
+/**
+ * https://www.codewars.com/kata/582cb0224e56e068d800003c/train/java
+ *
+ * @author tamercankacak
+ */
+public class KeepHydrated {
+  public static int Liters(double time) {
+    return (int) time / 2;
+  }
+}
