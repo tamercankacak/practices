ca3d4ed9aabe5bc8a36ca63f1d8862c13aaab084
tamer.kacak <tamercankacak@gmail.com>
2022-02-23 12:22:34 +0300

    chore: StringToNumber

---
+++ src/coderwars/StringToNumber.java
@@ -0,0 +1,12 @@
+package coderwars;
+
+/**
+ * https://www.codewars.com/kata/544675c6f971f7399a000e79/train/java
+ *
+ * @author tamercankacak
+ */
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    return Integer.parseInt(str);
+  }
+}
