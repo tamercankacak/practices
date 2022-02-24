8a9f2db95fc478b783599d87c23b72a05d69deae
tamer.kacak <tamercankacak@gmail.com>
2022-02-24 11:06:56 +0300

    chore: FindStrayNumber

---
+++ src/coderwars/FindStrayNumber.java
@@ -0,0 +1,10 @@
+package coderwars;
+
+
+public class FindStrayNumber {
+  static int stray(int[] numbers) {
+    if (numbers[0] != numbers[1] && numbers[0] != numbers[2]) return numbers[0];
+    for (int i : numbers) if (i != numbers[0]) return i;
+    return 0;
+  }
+}
