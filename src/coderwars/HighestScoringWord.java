7d414eaf3bf02a0df8f2fb46e8ab9113928e0b6e
tamer.kacak <tamercankacak@gmail.com>
2022-02-28 11:49:19 +0300

    chore: HighestScoringWord

---
+++ src/coderwars/HighestScoringWord.java
@@ -0,0 +1,12 @@
+package coderwars;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HighestScoringWord {
+  public static String high(String s) {
+    return Arrays.stream(s.split(" "))
+        .max(Comparator.comparingInt(a -> a.chars().map(i -> i - 96).sum()))
+        .get();
+  }
+}
