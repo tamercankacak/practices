d261c9a78879beadfe7828e3c85bd4dff2e8c58d
tamer.kacak <tamercankacak@gmail.com>
2022-03-06 11:47:00 +0300

    chore: BooleanToString

---
+++ src/coderwars/BooleanToString.java
@@ -0,0 +1,12 @@
+package coderwars;
+
+public class BooleanToString {
+  public static String convert(boolean b){
+    if(b) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+
+}
