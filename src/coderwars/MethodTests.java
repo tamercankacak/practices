096f3b970e659b106f9a3b2e9a81f63aebea6db5
tamer.kacak <tamercankacak@gmail.com>
2022-03-10 23:26:02 +0300

    chore: MethodTests

--- src/coderwars/MethodTests.java
+++ src/coderwars/MethodTests.java
@@ -21,20 +21,121 @@ import org.junit.Test;
 public class MethodTests {
 
   @Test
-  public void testBonus() {
-    boolean wellConfigured=unicodeTest();
-    assertEquals((wellConfigured?"£":"\u00A3")+"100000",Bonus.bonusTime(10000, true));
-    assertEquals((wellConfigured?"£":"\u00A3")+"250000",Bonus.bonusTime(25000, true));
-    assertEquals((wellConfigured?"£":"\u00A3")+"10000",Bonus.bonusTime(10000, false));
-    assertEquals((wellConfigured?"£":"\u00A3")+"60000",Bonus.bonusTime(60000, false));
-    assertEquals((wellConfigured?"£":"\u00A3")+"20",Bonus.bonusTime(2, true));
-    assertEquals((wellConfigured?"£":"\u00A3")+"78",Bonus.bonusTime(78, false));
-    assertEquals((wellConfigured?"£":"\u00A3")+"678900",Bonus.bonusTime(67890, true));
+  public void coderTest() {
+    List<Integer> arr =
+        new ArrayList<Integer>() {
+          {
+            add(1);
+            add(2);
+            add(3);
+          }
+        };
+
+    List<List<Integer>> operations =
+        new ArrayList<List<Integer>>() {
+          {
+            add(
+                new ArrayList<Integer>() {
+                  {
+                    add(2);
+                    add(0);
+                    add(2);
+                  }
+                });
+            add(
+                new ArrayList<Integer>() {
+                  {
+                    add(1);
+                    add(0);
+                    add(4);
+                  }
+                });
+            add(
+                new ArrayList<Integer>() {
+                  {
+                    add(2);
+                    add(0);
+                    add(2);
+                  }
+                });
+          }
+        };
+    List<Integer> result =
+        new ArrayList<Integer>() {
+          {
+            add(2);
+            add(2);
+          }
+        };
+    assertEquals(result, Coders2.evenOdd(arr, operations));
   }
 
-  public boolean unicodeTest(){
-    System.out.println("\u00A3 == £:"+"\u00A3".equalsIgnoreCase("£"));
-    System.out.println("if previous result was false or had ? symbol then perhaps you need to escape unicode due to misconfiguration");
+  @Test
+  public void basicTest1() {
+    String[] result = new String[] {"Hello", "hEllo", "heLlo", "helLo", "hellO"};
+    assertArrayEquals(
+        "it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave("hello"));
+    result =
+        new String[] {
+          "Codewars",
+          "cOdewars",
+          "coDewars",
+          "codEwars",
+          "codeWars",
+          "codewArs",
+          "codewaRs",
+          "codewarS"
+        };
+    assertArrayEquals(
+        "it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave("codewars"));
+    result = new String[] {};
+    assertArrayEquals(
+        "it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(""));
+    result =
+        new String[] {
+          "Two words",
+          "tWo words",
+          "twO words",
+          "two Words",
+          "two wOrds",
+          "two woRds",
+          "two worDs",
+          "two wordS"
+        };
+    assertArrayEquals(
+        "it should return '" + Arrays.toString(result) + "'",
+        result,
+        MexicanWave.wave("two words"));
+    result = new String[] {" Gap ", " gAp ", " gaP "};
+    assertArrayEquals(
+        "it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(" gap "));
+  }
+
+  @Test
+  public void testFixStringCase() {
+    assertEquals("code", FixStringCase.solve("code"));
+    assertEquals("CODE", FixStringCase.solve("CODe"));
+    assertEquals("code", FixStringCase.solve("COde"));
+    assertEquals("code", FixStringCase.solve("Code"));
+    assertEquals("", FixStringCase.solve(""));
+  }
+
+  @Test
+  public void testBonus() {
+    boolean wellConfigured = unicodeTest();
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "100000", Bonus.bonusTime(10000, true));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "250000", Bonus.bonusTime(25000, true));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "10000", Bonus.bonusTime(10000, false));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "60000", Bonus.bonusTime(60000, false));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "20", Bonus.bonusTime(2, true));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "78", Bonus.bonusTime(78, false));
+    assertEquals((wellConfigured ? "£" : "\u00A3") + "678900", Bonus.bonusTime(67890, true));
+  }
+
+  public boolean unicodeTest() {
+    System.out.println("\u00A3 == £:" + "\u00A3".equalsIgnoreCase("£"));
+    System.out.println(
+        "if previous result was false or had ? symbol then perhaps you need to escape unicode due to misconfiguration");
     return "\u00A3".equalsIgnoreCase("£");
   }
 
