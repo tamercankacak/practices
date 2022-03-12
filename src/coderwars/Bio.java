59cce6d22d537eb667c2e05c80dca27597382595
tamer.kacak <tamercankacak@gmail.com>
2022-03-12 23:52:51 +0300

    chore: Bio

---
+++ src/coderwars/Bio.java
@@ -0,0 +1,7 @@
+package coderwars;
+
+public class Bio {
+    public String dnaToRna(String dna) {
+        return dna.replace('T','U');  // Do your magic!
+    }
+}
--- src/coderwars/MethodTests.java
+++ src/coderwars/MethodTests.java
@@ -21,6 +21,15 @@ import org.junit.Test;
 public class MethodTests {
 
   @Test
+  public void testMinimum() {
+    assertEquals(13, Minimum.minValue(new int[] {1, 3, 1}));
+    assertEquals(457, Minimum.minValue(new int[] {4, 7, 5, 7}));
+    assertEquals(148, Minimum.minValue(new int[] {4, 8, 1, 4}));
+    assertEquals(579, Minimum.minValue(new int[] {5, 7, 9, 5, 7}));
+    assertEquals(678, Minimum.minValue(new int[] {6, 7, 8, 7, 6, 6}));
+  }
+
+  @Test
   public void coderTest() {
     List<Integer> arr =
         new ArrayList<Integer>() {
