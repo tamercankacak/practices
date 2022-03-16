1ab01691f416ebc2f77cf798de7ed3ee6cf654ee
tamer.kacak <tamercankacak@gmail.com>
2022-03-16 14:32:50 +0300

    chore: InOrder

---
+++ src/coderwars/InOrder.java
@@ -0,0 +1,10 @@
+package coderwars;
+
+public class InOrder {
+  public static boolean isAscOrder(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) return false;
+    }
+    return true;
+  }
+}
